e309499c17caa12cca53e46ebb511147db15cb93
techno-tanoC <develop.tanoc@gmail.com>
2016-05-09 22:44:48 +0900

    test app

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,15 @@
 module Main where
 
+import Control.Monad
+import Control.Concurrent.STM
 import Lib
 
 main :: IO ()
-main = return ()
-
+main = do
+  holder <- atomically newHolder
+  atomically $ forM_ [1..10] $ \i ->
+    insert i 0 holder
+  atomically $ forM_ [1..100000000] $ \i ->
+    modify succ (i `mod` 10) holder
+  Just n <- atomically $ get 1 holder
+  print n
